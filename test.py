ec188355cfb08a09d5b3eebfd7c7fef4eecc7cf6
bluesnogbox <mybluesnogbox@gmail.com>
2015-08-19 12:52:03 -0400

    update test to use MPDClient.search

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ client.timeout = 10
 client.idletimeout = None
 client.connect("localhost", 6600)
 
-result = client.listfiles('/Cake')
+result = client.search('any', 'Led Zeppelin')
 res_list = set()
 for i in result:
-        if 'directory' in i:
-            res_list.add(i['directory'])
+        if 'artist' in i:
+            res_list.add(i['artist'])
 for item in sorted(list(res_list)):
     print(item)                            
+#print(result)
 client.close
 client.disconnect
